d2be5d8596f6ef443c678a0b0a4523adc355b32f
DoctorDark <doctordark11@gmail.com>
2015-08-21 03:06:34 +0100

    Use Guava to suppress closeables

--- src/main/java/com/fuzzoland/BungeePortals/Listeners/EventListener.java
+++ src/main/java/com/fuzzoland/BungeePortals/Listeners/EventListener.java
@@ -1,6 +1,8 @@
 package com.fuzzoland.BungeePortals.Listeners;
 
 import com.fuzzoland.BungeePortals.BungeePortals;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -23,7 +25,7 @@ public class EventListener implements Listener {
     }
 
     @EventHandler
-    public void onPlayerMove(PlayerMoveEvent event) throws IOException {
+    public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
         String playerName = player.getName();
         if (!this.statusData.containsKey(playerName)) {
@@ -36,13 +38,10 @@ public class EventListener implements Listener {
                 this.statusData.put(playerName, true);
                 String destination = plugin.portalData.get(data);
                 if (player.hasPermission("BungeePortals.portal." + destination) || player.hasPermission("BungeePortals.portal.*")) {
-                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                    DataOutputStream dos = new DataOutputStream(baos);
-                    dos.writeUTF("Connect");
-                    dos.writeUTF(destination);
-                    player.sendPluginMessage(plugin, "BungeeCord", baos.toByteArray());
-                    baos.close();
-                    dos.close();
+                    ByteArrayDataOutput out = ByteStreams.newDataOutput();
+                    out.writeUTF("Connect");
+                    out.writeUTF(destination);
+                    player.sendPluginMessage(plugin, "BungeeCord", out.toByteArray());
                 } else {
                     player.sendMessage(plugin.configFile.getString("NoPortalPermissionMessage").replace("{destination}", destination).replaceAll("(&([a-f0-9l-or]))", "\u00A7$2"));
                 }
