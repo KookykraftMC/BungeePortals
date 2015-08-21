c713924fbdb836c1afba228a619cf336b818c611
DoctorDark <doctordark11@gmail.com>
2015-08-21 03:08:08 +0100

    Use a HashSet over a Map<?,Boolean>, store UUID instead of name

--- src/main/java/com/fuzzoland/BungeePortals/Listeners/EventListener.java
+++ src/main/java/com/fuzzoland/BungeePortals/Listeners/EventListener.java
@@ -1,6 +1,7 @@
 package com.fuzzoland.BungeePortals.Listeners;
 
 import com.fuzzoland.BungeePortals.BungeePortals;
+import com.google.common.collect.Sets;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 import org.bukkit.block.Block;
@@ -9,16 +10,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
 public class EventListener implements Listener {
 
-    private BungeePortals plugin;
-    private Map<String, Boolean> statusData = new HashMap<String, Boolean>();
+    private final BungeePortals plugin;
+    private final Set<UUID> statusData = Sets.newHashSet();
 
     public EventListener(BungeePortals plugin) {
         this.plugin = plugin;
@@ -27,29 +25,22 @@ public class EventListener implements Listener {
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
-        String playerName = player.getName();
-        if (!this.statusData.containsKey(playerName)) {
-            this.statusData.put(playerName, false);
-        }
         Block block = player.getWorld().getBlockAt(player.getLocation());
         String data = block.getWorld().getName() + "#" + String.valueOf(block.getX()) + "#" + String.valueOf(block.getY()) + "#" + String.valueOf(block.getZ());
         if (plugin.portalData.containsKey(data)) {
-            if (!this.statusData.get(playerName)) {
-                this.statusData.put(playerName, true);
-                String destination = plugin.portalData.get(data);
-                if (player.hasPermission("BungeePortals.portal." + destination) || player.hasPermission("BungeePortals.portal.*")) {
-                    ByteArrayDataOutput out = ByteStreams.newDataOutput();
-                    out.writeUTF("Connect");
-                    out.writeUTF(destination);
-                    player.sendPluginMessage(plugin, "BungeeCord", out.toByteArray());
-                } else {
-                    player.sendMessage(plugin.configFile.getString("NoPortalPermissionMessage").replace("{destination}", destination).replaceAll("(&([a-f0-9l-or]))", "\u00A7$2"));
-                }
+            this.statusData.add(player.getUniqueId());
+
+            String destination = plugin.portalData.get(data);
+            if (player.hasPermission("BungeePortals.portal." + destination) || player.hasPermission("BungeePortals.portal.*")) {
+                ByteArrayDataOutput out = ByteStreams.newDataOutput();
+                out.writeUTF("Connect");
+                out.writeUTF(destination);
+                player.sendPluginMessage(plugin, "BungeeCord", out.toByteArray());
+            } else {
+                player.sendMessage(plugin.configFile.getString("NoPortalPermissionMessage").replace("{destination}", destination).replaceAll("(&([a-f0-9l-or]))", "\u00A7$2"));
             }
         } else {
-            if (this.statusData.get(playerName)) {
-                this.statusData.put(playerName, false);
-            }
+            this.statusData.remove(player.getUniqueId());
         }
     }
 }
